2845d68185ffa031c5d541979bd9324f3bea60cf
Allan David <allanbrados@gmail.com>
2018-01-25 14:32:05 -0200

    Creates Money module to generate a %Money{}

--- lib/money.ex
+++ lib/money.ex
@@ -1,24 +1,75 @@
 defmodule FinancialSystem.Money do
   defstruct code: nil, decimal_points: nil, int_part: nil, dec_part: nil
 
-  def new(amount, _code, _decimal_points) when is_float(amount) do
-    case split_float_in_two_integers(amount) do
-      {:ok, {int_part, dec_part}} ->
-        {:ok, %FinancialSystem.Money{int_part: int_part, dec_part: dec_part}}
-
-      {:error, reason} ->
-        {:error, reason}
+  def new(amount, code, decimal_points) do
+    with {:ok, code} <- is_valid_code(code),
+         {:ok, decimal_points} <- is_valid_integer(decimal_points),
+         {:ok, decimal_points} <- is_positive_number(decimal_points),
+         {:ok, amount} <- is_valid_number(amount),
+         {:ok, amount} <- is_positive_number(amount),
+         {:ok, {int_part, dec_part}} <- split_number_in_two_integers(amount) do
+      {:ok, %FinancialSystem.Money{code: code, int_part: int_part, dec_part: dec_part, decimal_points: decimal_points}}
+    else
+      {:error, reason} -> {:error, reason}
     end
   end
 
-  defp split_float_in_two_integers(amount) do
-    with number <- Float.to_string(amount),
+  defp is_valid_integer(number) do
+    cond do
+      is_integer(number) ->
+        {:ok, number}
+
+      true ->
+        {:error, "Not a valid integer"}
+    end
+  end
+
+  defp is_valid_number(number) do
+    cond do
+      is_integer(number) ->
+        {:ok, number}
+
+      is_float(number) ->
+        {:ok, number}
+
+      true ->
+        {:error, "Not a valid number"}
+    end
+  end
+
+  defp is_positive_number(number) do
+    cond do
+      number >= 0 ->
+        {:ok, number}
+
+      number < 0 ->
+        {:error, "Money amount can't be negative"}
+
+      true ->
+        {:error, "Given number is invalid"}
+    end
+  end
+
+  defp is_valid_code(code) do
+    if is_binary(code) and String.length(code) == 3 do
+      {:ok, code}
+    else
+      {:error, "Invalid code"}
+    end
+  end
+
+  defp split_number_in_two_integers(number) when is_float(number) do
+    with number <- Float.to_string(number),
          {int_part, rest} <- Integer.parse(number),
          rest <- String.trim(rest, "."),
          {dec_part, _rest} <- Integer.parse(rest) do
       {:ok, {int_part, dec_part}}
     else
-      :error -> {:error, "Couldnt split amount into integers"}
+      :error -> {:error, "Couldnt split number into integers"}
     end
+  end
+
+  defp split_number_in_two_integers(number) when is_integer(number) do
+    {:ok, {number, 0}}
   end
 end
--- test/money_test.exs
+++ test/money_test.exs
@@ -6,16 +6,19 @@ defmodule MoneyTest do
 
   setup do
     {:ok,
-     recipient: %Money{
+     recipient: %{
        code: "ABC",
        decimal_points: 3,
        int_part: 42,
-       dec_part: 72
+       dec_part: 72,
+       value: 42.72
      }}
   end
 
+  #======Testing about the received amount=====#
+
   test "#new receiving a float should split it", state do
-    %Money{code: code, decimal_points: dec_points} = state.recipient
+    %{code: code, decimal_points: dec_points} = state.recipient
 
     amount = 42.5
 
@@ -25,14 +28,138 @@ defmodule MoneyTest do
     assert dec_part == 5
   end
 
-  # test "#new receiving a int should split it with dec_part 0", state do
-  #   %Money{code: code, decimal_points: dec_points} = state.recipient
+  test "#new receiving an int should split it with dec_part 0", state do
+    %{code: code, decimal_points: dec_points} = state.recipient
 
-  #   amount = 42
+    amount = 42
 
-  #   {:ok, %Money{int_part: int_part, dec_part: dec_part}} = Money.new(amount, code, dec_points)
+    {:ok, %Money{int_part: int_part, dec_part: dec_part}} = Money.new(amount, code, dec_points)
 
-  #   assert int_part == 42
-  #   assert dec_part == 0
-  # end
+    assert int_part == 42
+    assert dec_part == 0
+  end
+
+  test "#new receiving a negative float should return error", state do
+    %{code: code, decimal_points: dec_points} = state.recipient
+
+    amount = -42.5
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving a negative int should return error", state do
+    %{code: code, decimal_points: dec_points} = state.recipient
+
+    amount = -42
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving a string in amount should return error", state do
+    %{code: code, decimal_points: dec_points} = state.recipient
+
+    amount = "teste"
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving nil in amount should return error", state do
+    %{code: code, decimal_points: dec_points} = state.recipient
+
+    amount = "teste"
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  #======Testing about the received code=====#
+
+  test "#new receiving valid code should return ok", state do
+    %{value: amount, decimal_points: dec_points} = state.recipient
+
+    code = "BRL"
+
+    {:ok, %Money{code: resp_code}} = Money.new(amount, code, dec_points)
+
+    assert resp_code == code
+  end
+
+  test "#new receiving 4 digits code should return error", state do
+    %{value: amount, decimal_points: dec_points} = state.recipient
+
+    code = "BRLA"
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving 2 digits code should return error", state do
+    %{value: amount, decimal_points: dec_points} = state.recipient
+
+    code = "BR"
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving empty string code should return error", state do
+    %{value: amount, decimal_points: dec_points} = state.recipient
+
+    code = ""
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  #======Testing about the received decimal_points=====#
+
+  test "#new receiving float should return error", state do
+    %{value: amount, code: code} = state.recipient
+
+    dec_points = 2.5
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving string should return error", state do
+    %{value: amount, code: code} = state.recipient
+
+    dec_points = "2.5"
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving negative integer should return error", state do
+    %{value: amount, code: code} = state.recipient
+
+    dec_points = -2
+
+    {resp, _response} = Money.new(amount, code, dec_points)
+
+    assert resp == :error
+  end
+
+  test "#new receiving positive integer should return ok", state do
+    %{value: amount, code: code} = state.recipient
+
+    dec_points = 2
+
+    {:ok, %Money{decimal_points: resp_points}} = Money.new(amount, code, dec_points)
+
+    assert resp_points == 2
+  end
 end
